31058424c6d889e042f2adf4f6e63965e690cf09
Dan <bodepd@gmail.com>
2014-08-21 14:32:58 +0530

    add reliance_spawn_machine role
    Which can be used to configure out a machine that
    can run the spawn_resources script.

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,16 @@
 
 if $::role == 'tempest' {
   include tempest
+} elsif $::role == 'reliance_spawn_machine' {
+  package { 'git': }
+  package { 'vim': }
+  class { 'openstack_extras::repo::uca':
+    release => 'havana',
+  }
+  class { 'openstack_extras::client':
+    ceilometer => false,
+  }
 } else {
-  include jiocloud
+  fail("Undefined role ${::role}")
+  #include jiocloud
 }
